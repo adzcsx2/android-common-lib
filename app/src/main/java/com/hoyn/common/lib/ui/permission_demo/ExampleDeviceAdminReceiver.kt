b18d49baca79760308d1544f145f5fb2a025c1ee
hoyn <adzcsx2@gmail.com>
2026-04-13 14:12:25 +0800

    feat: 新增设备管理器 Receiver Stub 用于演示设备管理器权限请求

---
+++ app/src/main/java/com/hoyn/common/lib/ui/permission_demo/ExampleDeviceAdminReceiver.kt
@@ -0,0 +1,8 @@
+package com.hoyn.common.lib.ui.permission_demo
+
+import android.app.admin.DeviceAdminReceiver
+
+/**
+ * 设备管理器 Receiver Stub，仅用于演示设备管理器权限请求
+ */
+class ExampleDeviceAdminReceiver : DeviceAdminReceiver()
