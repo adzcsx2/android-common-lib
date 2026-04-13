ff2f1804fbc4ef08c51c8f34458171fa2d30d63e
hoyn <adzcsx2@gmail.com>
2026-04-13 14:15:00 +0800

    feat: 新增通知监听服务 Stub 用于演示通知监听权限请求

---
+++ app/src/main/java/com/hoyn/common/lib/ui/permission_demo/ExampleNotificationListenerService.kt
@@ -0,0 +1,8 @@
+package com.hoyn.common.lib.ui.permission_demo
+
+import android.service.notification.NotificationListenerService
+
+/**
+ * 通知监听服务 Stub，仅用于演示通知监听权限请求
+ */
+class ExampleNotificationListenerService : NotificationListenerService()
